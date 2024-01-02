6fdc5e16178977e2b455b2a7f29b310b3c0718da
Developerraju23 <rajukumaraju.rk@gmail.com>
2024-01-02 15:22:37 +0600

    first commit

---
+++ first-program/index.js
@@ -0,0 +1,9 @@
+var userName = "Raju Halder";
+console.log(userName);
+// Function 
+function addNumber(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumber(30, 50);
+addNumber(50, 50);
+console.log(addNumber);
---
+++ first-program/index.ts
@@ -0,0 +1,10 @@
+let userName = "Raju Halder";
+console.log(userName)
+
+// Function 
+function addNumber (num1: number, num2:number){
+    console.log(num1 + num2)
+}
+addNumber(30 ,50)
+addNumber(50 ,50)
+console.log(addNumber)
